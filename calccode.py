b560c5fec0e0828f6e1f05721f2528ffd194705b
steven <steven.immune@gmail.com>
2023-03-12 21:09:39 -0700

    clac code python

---
+++ calccode.py
@@ -0,0 +1,22 @@
+class Calc:
+    def sum(Self , x,y):
+        return x+y
+    
+    def mul(Self, x,y):
+        return x*y
+
+
+
+
+c1 = Calc()
+print(c1.sum(10,20))
+
+print(c1.mul(10,20))
+
+
+
+
+c2 = Calc ()
+print(c2.sum(10,80))
+
+print(c2.mul(456,2))
